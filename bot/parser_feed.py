a40d9ef33f3bc4b155762c05cbde05d9f7a0ad08
gorandp <gorandp@outlook.com>
2023-08-17 23:26:52 -0300

    develop new scraper + rename old site codeblock - reutilize old code - local test

--- bot/parser_feed.py
+++ bot/parser_feed.py
@@ -6,14 +6,20 @@ import re
 from .base import Base
 
 
-TIMEOUT = int(os.getenv('TIMEOUT'))
+class OLD_SITE:
+    TIMEOUT = int(os.getenv('TIMEOUT'))
+    HISTORIC_FEED_URL = "https://wwwsi.frsn.utn.edu.ar/frsn/selec_seccion.asp?"\
+                        "IDSeccion=196&IDSub=197&ContentID=300"
+    SHORT_FEED_URL = "https://wwwsi.frsn.utn.edu.ar/frsn/index_posta.asp"
+    REGEX_HISTORIC_FEED = (r"selec_seccion\.asp\?IDSeccion=\d+&IDSub=\d+&"
+                           r"ContentID=\d+")
 
-HISTORIC_FEED_URL = "https://wwwsi.frsn.utn.edu.ar/frsn/selec_seccion.asp?"\
-                    "IDSeccion=196&IDSub=197&ContentID=300"
-SHORT_FEED_URL = "https://wwwsi.frsn.utn.edu.ar/frsn/index_posta.asp"
 
-REGEX_HISTORIC_FEED = (r"selec_seccion\.asp\?IDSeccion=\d+&IDSub=\d+&"
-                       r"ContentID=\d+")
+class WORDPRESS_SITE:
+    TIMEOUT = int(os.getenv('TIMEOUT'))
+    HISTORIC_FEED_URL = "https://www.frsn.utn.edu.ar/?paged=1&page_id=80"
+    REGEX_HISTORIC_FEED = r"/\?p=\d+"
+    PAGINATION_URL = "https://www.frsn.utn.edu.ar/?paged={}&page_id=80"
 
 
 class HistoricFeed(Base):
@@ -21,14 +27,43 @@ class HistoricFeed(Base):
         super().__init__(__name__)
 
     def get_urls(self, limit: int = None):
-        response = requests.get(HISTORIC_FEED_URL, timeout=TIMEOUT)
+        response = requests.get(
+            WORDPRESS_SITE.HISTORIC_FEED_URL, timeout=WORDPRESS_SITE.TIMEOUT)
+        soup = bs4.BeautifulSoup(
+            response.content.decode("utf-8", errors="ignore"),
+            "lxml"
+        )
+        urls = []
+        last_page = int(soup.select("nav > div > a.page-numbers")[-2].text)
+        for page in range(1, last_page+1):
+            if page > 1:
+                response = requests.get(
+                    WORDPRESS_SITE.PAGINATION_URL.format(page),
+                    timeout=WORDPRESS_SITE.TIMEOUT)
+                soup = bs4.BeautifulSoup(
+                    response.content.decode("utf-8", errors="ignore"),
+                    "lxml"
+                )
+            for index, new in enumerate(soup.select("article.post")):
+                if limit is not None and index >= limit:
+                    break
+                new_url = new.find("a").attrs["href"]
+                photo_url = new.find("img")
+                if photo_url:
+                    photo_url = photo_url.attrs["src"]
+                urls.append((new_url, photo_url))
+        return urls
+
+    def get_urls_old_site(self, limit: int = None):
+        response = requests.get(
+            OLD_SITE.HISTORIC_FEED_URL, timeout=OLD_SITE.TIMEOUT)
         parser = bs4.BeautifulSoup(
             response.content.decode("utf-8", errors="ignore"),
             "lxml"
         )
         urls = []
         for index, new in enumerate(parser.find_all(
-            "a", attrs={'href': re.compile(REGEX_HISTORIC_FEED)}
+            "a", attrs={'href': re.compile(OLD_SITE.REGEX_HISTORIC_FEED)}
         )):
             if limit is not None and index >= limit:
                 break
@@ -52,7 +87,8 @@ class ShortFeed(Base):
         super().__init__(__name__)
 
     def get_urls(self):
-        response = requests.get(SHORT_FEED_URL, timeout=TIMEOUT)
+        response = requests.get(
+            OLD_SITE.SHORT_FEED_URL, timeout=OLD_SITE.TIMEOUT)
         parser = bs4.BeautifulSoup(
             response.content.decode("utf-8", errors="ignore"),
             "lxml"
--- bot/parser_news.py
+++ bot/parser_news.py
@@ -19,6 +19,33 @@ class NewsReader(Base):
         response = requests.get(url, timeout=TIMEOUT)
         self.logger.debug(f"Parsing: {url}")
         parse_time = time()
+        soup = bs4.BeautifulSoup(
+            response.content.decode("utf-8", errors="ignore"),
+            "lxml"
+        )
+        title = soup.find("h1")
+        body = soup.find("main > article")
+        title = title.text.strip() if title else ""
+        body = body.text.strip() if body else ""
+        new = {
+            "insertedDatetime": datetime.utcnow(),
+            "url": url,
+            "title": title,
+            "body": body,
+            "urlPhoto": url_photo,
+            "responseElapsedTime": response.elapsed.total_seconds(),
+            "parseElapsedTime": time() - parse_time,
+        }
+        if not url_photo:
+            del new['urlPhoto']
+        self.logger.debug(f"Done parse of: {url}")
+        return new
+
+    def read_new_old_site(self, url: str, url_photo: str):
+        self.logger.debug(f"Getting: {url}")
+        response = requests.get(url, timeout=TIMEOUT)
+        self.logger.debug(f"Parsing: {url}")
+        parse_time = time()
         parser = bs4.BeautifulSoup(
             response.content.decode("utf-8", errors="ignore"),
             "lxml"
