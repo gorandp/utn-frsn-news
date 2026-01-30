7599d12abd4f89d9dc4198a10b65d31a5f2349be
Goran Prpic <gorandp@outlook.com>
2026-01-30 18:09:55 -0300

    worker entry script updated with db connection

--- src/entry.py
+++ src/entry.py
@@ -1,10 +1,12 @@
-from workers import Response, WorkerEntrypoint
-
-
-def get_hello_message():
-    return "Hello World!"
+from workers import Response, WorkerEntrypoint, Request
+from sqlalchemy_cloudflare_d1 import create_engine_from_binding  # type: ignore
+from sqlalchemy.orm import sessionmaker, Session
 
 
 class Default(WorkerEntrypoint):
-    async def fetch(self, request):
-        return Response(get_hello_message())
+    async def fetch(self, request: Request):
+        engine = create_engine_from_binding(self.env.DB)  # type: ignore
+        SessionLocal = sessionmaker(bind=engine)
+        with SessionLocal() as session:
+            pass  # Main logic would go here
+        return Response("Success", status=200)
