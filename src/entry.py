2fb4d0e5ba5471f94464073dd30b76faee3ec845
Goran Prpic <gorandp@outlook.com>
2026-01-30 18:19:16 -0300

    worker entry update

--- src/entry.py
+++ src/entry.py
@@ -1,6 +1,9 @@
 from workers import Response, WorkerEntrypoint, Request
 from sqlalchemy_cloudflare_d1 import create_engine_from_binding  # type: ignore
 from sqlalchemy.orm import sessionmaker, Session
+from pyodide.ffi import to_js
+from asyncio import sleep
+from datetime import datetime, UTC
 
 
 class Default(WorkerEntrypoint):
@@ -8,5 +11,34 @@ class Default(WorkerEntrypoint):
         engine = create_engine_from_binding(self.env.DB)  # type: ignore
         SessionLocal = sessionmaker(bind=engine)
         with SessionLocal() as session:
-            pass  # Main logic would go here
+            if request.url.endswith("/start/indexScraper"):
+                # TODO: Index Scraper call
+                news_urls = []
+                # Queue insert
+                for url in news_urls:
+                    await self.env.SCRAPER_QUEUE.send(
+                        to_js(
+                            {
+                                "url": request.url,
+                                "inserted_at": datetime.now(UTC).isoformat(),
+                            }
+                        )
+                    )
+                    await sleep(1)  # Throttle
+            elif request.url.endswith("/start/mainScraper"):
+                # TODO: Main Scraper call
+                news_url = ""
+                await self.env.MESSENGER_QUEUE.send(
+                    to_js(
+                        {
+                            "news_url": news_url,
+                            "inserted_at": datetime.now(UTC).isoformat(),
+                        }
+                    )
+                )
+            elif request.url.endswith("/start/messenger"):
+                # TODO: Messenger call
+                pass
+            else:
+                return Response("Not Found", status=404)
         return Response("Success", status=200)
