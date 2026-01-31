6627c8880f51a4c9471987da48bcdd2bf98befb2
Goran Prpic <gorandp@outlook.com>
2026-01-31 12:49:52 -0300

    parser news use async pyfetch + remove old code

--- src/app/scraper/parser_news.py
+++ src/app/scraper/parser_news.py
@@ -1,8 +1,8 @@
-from time import time
 import os
-import requests
 import bs4
-from datetime import datetime
+from pyodide.http import pyfetch
+from datetime import datetime, UTC
+from time import time
 
 from ..logger import LogWrapper
 
@@ -11,17 +11,22 @@ TIMEOUT = int(os.getenv("TIMEOUT") or "60")
 
 
 class NewsReader(LogWrapper):
-    def read_new(
+    async def read_new(
         self,
         url: str,
         url_photo: str,
     ):
-        self.logger.debug(f"Getting: {url}")
-        response = requests.get(url, timeout=TIMEOUT)
-        self.logger.debug(f"Parsing: {url}")
+        # self.logger.debug(f"Getting: {url}")
+        fetch_time = time()
+        # TODO: abort fetch after TIMEOUT is reached
+        response = await pyfetch(url)
+        content = await response.bytes()
+        fetch_time = time() - fetch_time
+        # self.logger.debug(f"Parsing: {url}")
         parse_time = time()
         soup = bs4.BeautifulSoup(
-            response.content.decode("utf-8", errors="ignore"), "lxml"
+            content.decode("utf-8", errors="ignore"),
+            "html.parser",
         )
         title = soup.find("h1")
         body = soup.select_one("div.entry-content")
@@ -29,46 +34,20 @@ class NewsReader(LogWrapper):
         title = title.text.strip() if title else ""
         body = body.text.replace("\n", "\n\n").strip() if body else ""
         new = {
-            "publishedDatetime": datetime.fromisoformat(entry_date.attrs["datetime"])
+            "publishedDatetime": datetime.fromisoformat(
+                str(entry_date.attrs["datetime"])
+            )
             if entry_date
             else None,
-            "insertedDatetime": datetime.utcnow(),
+            "insertedDatetime": datetime.now(UTC),
             "url": url,
             "title": title,
             "body": body,
             "urlPhoto": url_photo,
-            "responseElapsedTime": response.elapsed.total_seconds(),
+            "responseElapsedTime": fetch_time,
             "parseElapsedTime": time() - parse_time,
         }
         if not url_photo:
             del new["urlPhoto"]
-        self.logger.debug(f"Done parse of: {url}")
+        # self.logger.debug(f"Done parse of: {url}")
         return new
-
-    # def read_new_old_site(self, url: str, url_photo: str):
-    #     self.logger.debug(f"Getting: {url}")
-    #     response = requests.get(url, timeout=TIMEOUT)
-    #     self.logger.debug(f"Parsing: {url}")
-    #     parse_time = time()
-    #     parser = bs4.BeautifulSoup(
-    #         response.content.decode("utf-8", errors="ignore"), "lxml"
-    #     )
-    #     title = parser.find("td", attrs={"class": "textotitulo"})
-    #     body = parser.find("table", attrs={"class": "textohome"})
-    #     title = title.text.strip() if title else ""
-    #     body = body.text.strip() if body else ""
-    #     new = {
-    #         "insertedDatetime": datetime.utcnow(),
-    #         "url": url,
-    #         "title": title,
-    #         "body": body,
-    #         "urlPhoto": url_photo,
-    #         "responseElapsedTime": response.elapsed.total_seconds(),
-    #         "parseElapsedTime": time() - parse_time,
-    #     }
-    #     if url_photo:
-    #         new["urlPhoto"] = url_photo
-    #     else:
-    #         del new["urlPhoto"]
-    #     self.logger.debug(f"Done parse of: {url}")
-    #     return new
