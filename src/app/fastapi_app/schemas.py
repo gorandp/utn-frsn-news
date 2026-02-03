7768020e06f990fba448943fa855016a5f4da3c4
Goran Prpic <gorandp@outlook.com>
2026-02-03 10:39:13 -0300

    schemas init

---
+++ src/app/fastapi_app/schemas.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class NewsBase(BaseModel):
+    url: str = Field(min_length=1, max_length=511)
+    title: str = Field(min_length=1, max_length=127)
+    content: str = Field(min_length=1)
+    photo_url: str | None = Field(default=None, max_length=127)
+
+
+class NewsCreate(NewsBase):
+    pass
+
+
+class NewsResponse(NewsBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    response_elapsed_seconds: float | None = None
+    parse_elapsed_seconds: float | None = None
+    origin_created_at: datetime | None = None
+    indexed_at: datetime
+    inserted_at: datetime
